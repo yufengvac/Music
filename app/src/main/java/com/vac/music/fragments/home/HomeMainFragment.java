c80ebc05fcbe245e9ecbb90783cb4d5399f7bcdc
yufengvac@163.com <yufengvac@163.com>
2016-08-06 18:55:11 +0800

    upgrade my scrollview

--- app/src/main/java/com/vac/music/fragments/home/HomeMainFragment.java
+++ app/src/main/java/com/vac/music/fragments/home/HomeMainFragment.java
@@ -1,13 +1,18 @@
 package com.vac.music.fragments.home;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.vac.music.R;
@@ -20,12 +25,15 @@ import java.util.List;
  * Created by vac on 16/8/5.
  *
  */
-public class HomeMainFragment extends BaseSwipeBackFragment {
+public class HomeMainFragment extends BaseSwipeBackFragment implements LocalMusicFragment.onFragmentScrollViewListener {
 
+    private static final String TAG = HomeMainFragment.class.getSimpleName();
     private ViewPager viewPager;
     private TextView optionView1,optionView2;
     private FragmentPagerAdapter fragmentPagerAdapter;
+    private View indicatorView;
     private List<Fragment> fragmentList = new ArrayList<>();
+    private RelativeLayout topLaout;
     public static HomeMainFragment newInstance(Bundle bundle){
         HomeMainFragment homeMainFragment = new HomeMainFragment();
         if (bundle!=null){
@@ -35,13 +43,17 @@ public class HomeMainFragment extends BaseSwipeBackFragment {
     }
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(final LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.home_main_fragment,container,false);
         viewPager = (ViewPager) view.findViewById(R.id.home_main_fragment_viewPager);
         optionView1 = (TextView)view.findViewById(R.id.home_main_fragment_option1);
         optionView2 = (TextView)view.findViewById(R.id.home_main_fragment_option2);
+        topLaout = (RelativeLayout)view.findViewById(R.id.home_main_top_layout);
+        indicatorView = view.findViewById(R.id.home_main_indicator_view);
 
-        fragmentList.add(LocalMusicFragment.newInstance(null));
+        LocalMusicFragment localMusicFragment = LocalMusicFragment.newInstance(null);
+        localMusicFragment.setOnScrollListener(this);
+        fragmentList.add(localMusicFragment);
         fragmentList.add(NetMusicFragment.newInstance(null));
         fragmentPagerAdapter = new FragmentPagerAdapter(getFragmentManager()) {
             @Override
@@ -55,6 +67,67 @@ public class HomeMainFragment extends BaseSwipeBackFragment {
             }
         };
         viewPager.setAdapter(fragmentPagerAdapter);
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            ColorDrawable colorDrawable= (ColorDrawable) topLaout.getBackground();//获取背景颜色
+            int currentColor = colorDrawable.getColor();
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                LinearLayout.LayoutParams params = (android.widget.LinearLayout.LayoutParams) indicatorView
+                        .getLayoutParams();
+                params.setMargins(
+                        (int) (optionView1.getWidth() * (positionOffset + position)), 0,
+                        0, 0);
+                indicatorView.setLayoutParams(params);
+
+                if (positionOffsetPixels>0){
+
+                    int red = (currentColor & 0xff0000) >> 16;
+                    int green = (currentColor & 0x00ff00) >> 8;
+                    int blue = (currentColor & 0x0000ff);
+                    Log.i(TAG,"red="+red+",green="+green+",blue="+blue);
+
+                    int a = 0x30+(int)((0xff-0x30)*positionOffset);
+                    int r = (int)((0xbb)*positionOffset);
+                    int g = (int)((0x55)*positionOffset);
+                    int b = 0x0;
+                    color = Color.argb(a,r,g,b);
+                    topLaout.setBackgroundColor(color);
+                }
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+
         return attachToSwipeBack(view);
+    }
+
+    int color= Color.argb(0x90, 0xff, 0x00, 0x00);
+    int baseColor = 240;
+    int baseHeight = 250;
+    @Override
+    public void onScroll(int scrollY) {
+        if (scrollY<=baseHeight&&scrollY>0){
+            float scale = scrollY*1f/baseHeight;
+            int a = 0x30+(int)((0xff-0x30)*scale);
+            int r = (int)((0xbb)*scale);
+            int g = (int)((0x55)*scale);
+            int b = 0x0;
+            color = Color.argb(a,r,g,b);
+        }else if (scrollY>baseHeight){
+            color= Color.argb(0xff,0xbb,0x55,0x00);
+        }else if (scrollY==0){
+            color = Color.argb(0x30,0,0,0);
+        }
+        topLaout.setBackgroundColor(color);
     }
 }
--- app/src/main/java/com/vac/music/fragments/home/LocalMusicFragment.java
+++ app/src/main/java/com/vac/music/fragments/home/LocalMusicFragment.java
@@ -2,6 +2,7 @@ package com.vac.music.fragments.home;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,21 +12,28 @@ import android.widget.TextView;
 import com.vac.music.R;
 import com.vac.music.fragments.BaseSwipeBackFragment;
 import com.vac.music.fragments.test.TestFragment;
+import com.vac.music.myview.MyScrollView;
 
 /**
  * Created by vac on 16/8/5.
  *
  */
 public class LocalMusicFragment extends BaseSwipeBackFragment {
+    private static final String TAG = LocalMusicFragment.class.getSimpleName();
     private TextView textView;
 
     private RelativeLayout module_1;
+    private MyScrollView scrollView;
+    private onFragmentScrollViewListener onFragmentScrollViewListener;
     public static LocalMusicFragment newInstance(Bundle bundle){
         LocalMusicFragment localMusicFragment = new LocalMusicFragment();
         if (bundle!=null){
             localMusicFragment.setArguments(bundle);
         }
         return localMusicFragment;
+    }
+    public void setOnScrollListener(onFragmentScrollViewListener listener){
+        this.onFragmentScrollViewListener = listener;
     }
     @Nullable
     @Override
@@ -41,7 +49,7 @@ public class LocalMusicFragment extends BaseSwipeBackFragment {
 //                }
 //            }
 //        });
-        module_1 =(RelativeLayout) view.findViewById(R.id.my_music_fra_module_1);
+        module_1 =(RelativeLayout) view.findViewById(R.id.local_music_fra_module_1);
         module_1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -51,6 +59,19 @@ public class LocalMusicFragment extends BaseSwipeBackFragment {
                 }
             }
         });
+
+        scrollView = (MyScrollView) view.findViewById(R.id.local_music_fra_scrollView);
+        scrollView.setOnScrollListener(new MyScrollView.OnScrollListener() {
+            @Override
+            public void onScroll(int scrollY) {
+               onFragmentScrollViewListener.onScroll(scrollY);
+            }
+        });
         return attachToSwipeBack(view);
     }
+
+    public interface onFragmentScrollViewListener{
+        void onScroll(int scrollY);
+    }
+
 }
